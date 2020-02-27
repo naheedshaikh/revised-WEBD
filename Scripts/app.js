0244be563e7cdfcaeb5b03ff42d3fca5dd23fd57
Naheed Shaikh <naheed.shaikh@dcmail.ca>
2020-02-26 20:02:20 -0500

    initial commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -7,95 +7,42 @@
   
     let h2;
 
-     //event handlers
-     function Mouseover()
-     {
-         h2.style.color  = "green";
-         h2.style.fontSize = "80px";
-     }
+    // event handlers
+    function MouseOver()
+    {
+        h2.style.color="green";
+        h2.style.fontSize = "20px";
+    }
+    function MouseOut()
+    {
+        h2.style.color = "black";
+        h2.style.fontSize = "40px";
+    }
 
-     function Mouseout()
-     {
-         h2.style.color = "black";
-         h2.style.fontSize = "32px";
-        
-     }
- 
-    
     function Start()
     {
         console.log("%cApp Started...","color:blue; font-size:20px");
-       
-        //get a refrence to the contentArea
+
+        //get a refrence to the content area
         let contentArea = document.getElementById("contentArea");
 
-        //create an h2 tag
+        //Create an h2 element
         h2 = document.createElement("h2");
+        //add a class attributes to the h2 element
+        h2.setAttribute("class", "display-6");
+        h2.textContent="A secondary header";
 
-        //add a class attribute
-        h2.setAttribute("class","display-6");
-        h2.textContent = "a secondary header";
-
-        //append the new node to the content area
+        //append the new node to  the content area
         contentArea.appendChild(h2);
 
-        //inline function for event handler that we use for one time
         h2.addEventListener("click", function(){
-           // h2.style.color = "blue";
-           //To toggle
-           h2.style.color = (h2.style.color == "blue") ? "black" : "blue";
+            h2.style.color=(h2.style.color=="blue") ? "black" : "blue";
         });
 
-        h2.addEventListener("mouseover", Mouseover);
-        h2.addEventListener("mouseout", Mouseout);
+        h2.addEventListener("mouseover", MouseOver);
 
-        //creating a new paragraph element
-        let firstParagraph = document.createElement("p");
-
-        //configuring the new element
-        firstParagraph.textContent="This is a very short Paragraph";
-        
-        //get a refrence to the parentNode of the h2 element
-        let parentDiv = h2.parentNode;
-
-        //insert the new paragraph before the h2 element
-        parentDiv.insertBefore(firstParagraph, h2);
-
-        //get a refrence to the first h1 tag
-        let h1 = document.getElementsByTagName("h1")[0];
-        
-        //copy by value from one element to another
-        let anothetParagraph =  firstParagraph.cloneNode(true);
-
-        //move the paragraph above the h1
-        h1.parentNode.insertBefore(anothetParagraph, h1);
-
-        let ul = document.getElementsByTagName("ul")[0];
-        console.log(ul);
-
-    //     //hide li navbar elemets
-    //    for(let index = 0; index < ul.children.length; index++)
-    //     {
-    //           let child =  ul.children[index];
-    //           child.style.display = "none"
-    //     }
-    
-    //to hide all the elements from nav bar
-    /* for (const child of ul.children) 
-    {
-        child.style.display = "none";
-    } */
-    //to hide only last element      of nav bar
-    //ul.lastElementChild.style.display = "none";
-
-    ul.lastElementChild.style.fontWeight = "bold";
-    ul.lastElementChild.lastElementChild.style.color = "green";
-
-    let neel = new Student("Neel", 30, "1234567a");
-    neel.saysHello();
-    neel.isStudying();
-
+        h2.addEventListener("mouseout", MouseOut);
     }
-    //https://github.com/naheedshaikh/revised-WEBD.git
+
     window.addEventListener("load", Start);
 })();
